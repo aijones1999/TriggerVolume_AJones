ccbd8f56195b77cc6c412d2f846ebc42cb79a78e
Abby Jones <144266613+aijones1999@users.noreply.github.com>
2023-11-08 12:02:34 -0400

    Initial commit

---
+++ Assets/Scripts/MusicPause.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class MusicPause : MonoBehaviour
+{
+    public AudioSource MusicPlayer;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //Debug.Log(other.gameObject.name);
+
+        MusicPlayer.Pause();
+    }
+
+
+
+}
