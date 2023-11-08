a7e9663f0800fae942c531edb3fefe5730c647d2
Abby Jones <144266613+aijones1999@users.noreply.github.com>
2023-11-08 12:18:55 -0400

    Both Triggers Completed

---
+++ Assets/Scripts/TriggerPlaySFX.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerPlaySFX : MonoBehaviour
+{
+    public AudioSource SFX;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        SFX.Play();
+    }
+}
